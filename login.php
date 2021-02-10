497a37237f2dbab37c459a67e2c873aeae6f45a5
abhivikrant <vikrantabhishek72@gmail.com>
2021-02-10 23:50:25 +0530

    login.php coded.

---
+++ login.php
@@ -0,0 +1,25 @@
+<div class="body">
+		<div class="veen">
+			<div class="login-btn splits">
+				<p>Already an user?</p>
+				<button class="active">Login</button>
+			</div>
+			<div class="rgstr-btn splits">
+				<p>Don't have an account?</p>
+				<button>Register</button>
+			</div>
+			<div class="wrapper">
+				<form id="login" tabindex="500">
+					<h3>Login</h3>
+					<div class="mail">
+						<input type="mail" name="">
+						<label>Mail or Username</label>
+					</div>
+					<div class="passwd">
+						<input type="password" name="">
+						<label>Password</label>
+					</div>
+					<div class="submit">
+						<button class="dark">Login</button>
+					</div>
+				</form>
